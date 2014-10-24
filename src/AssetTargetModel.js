507c3892596fc1709f19020c0634cd2fc1ddf3be
F483 <fabian.barkhau@gmail.com>
2014-10-24 19:46:31 +0200

    fixed AssetTargetModel.address

--- src/AssetTargetModel.js
+++ src/AssetTargetModel.js
@@ -1,23 +1,24 @@
 
-function AssetTargetModel(assetTarget) {
-  this.address = assetTarget.getAddress()
-  var value = assetTarget.getValue(),
-    asset = assetTarget.getAsset()
+var bitcoin = require('coloredcoinjs-lib').bitcoin
 
-  this.formattedValue = asset.formatValue(value)
-  this.assetMoniker = asset.getMonikers()[0]
+function AssetTargetModel(assetTarget) {
+  var value = assetTarget.getValue();
+  var asset = assetTarget.getAsset();
+  this.address = assetTarget.getAddress().toBase58Check();
+  this.formattedValue = asset.formatValue(value);
+  this.assetMoniker = asset.getMonikers()[0];
 }
 
 AssetTargetModel.prototype.getAddress = function () {
-  return this.address
+  return this.address;
 }
 
 AssetTargetModel.prototype.getAssetMoniker = function () {
-  return this.assetMoniker
+  return this.assetMoniker;
 }
 
 AssetTargetModel.prototype.getFormattedValue = function () {
-  return this.formattedValue
+  return this.formattedValue;
 }
 
 module.exports = AssetTargetModel;
