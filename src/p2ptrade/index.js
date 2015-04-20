13eee534e7b63eaa607ae206f421556165f77dbe
F483 <fabian.barkhau@gmail.com>
2015-04-20 17:39:22 +0200

    factored out p2ptrade

--- src/p2ptrade/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ProtocolObjects: require('./ProtocolObjects'),
-  Comm: require('./Comm'),
-  EWCtrl: require('./EWCtrl'),
-  Utils: require('./Utils'),
-  Agent: require('./Agent')
-}
